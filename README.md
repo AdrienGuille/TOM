01dd5487d498bebd4164f299b8b65c87ea7b051f
Adrien Guille <adrien.guille@univ-lyon2.fr>
2016-02-05 16:39:15 +0100

    Add a sample program to generate a topic model browser

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ TOM (TOpic Modeling) is a Python 2.7 library for topic modeling and browsing. It
 
 ## Installation
 
-We recommend you to install Anaconda (https://www.continuum.io) which will automatically install most of the required dependencies (i.e. pandas, numpy, scipy, scikit-learn, matplotlib, nltk). You should then install the gensim module (https://anaconda.org/anaconda/gensim) and install nltk data (http://www.nltk.org/data.html). 
+We recommend you to install Anaconda (https://www.continuum.io) which will automatically install most of the required dependencies (i.e. pandas, numpy, scipy, scikit-learn, matplotlib, nltk, flask). You should then install the gensim module (https://anaconda.org/anaconda/gensim) and install nltk data (http://www.nltk.org/data.html). 
 If you intend to use the French lemmatizer, you should also install MElt on your system (https://www.rocq.inria.fr/alpage-wiki/tiki-index.php?page=MElt).
 Eventually, clone or download this repo and run the following command:
 
@@ -13,6 +13,8 @@ python setup.py install
 ```
 
 ## Usage
+
+We provide two sample programs, topic_model.py (which shows you how to load and prepare a corpus, estimate the optimal number of topics, infer the topic model and then manipulate it) and topic_model_browser.py (which shows you how to generate a topic model browser to explore a corpus), to help you get started using TOM.
 
 ###Load and prepare a text corpus
 
--- topic_model_browser.py
+++ topic_model_browser.py
@@ -1,33 +1,31 @@
 # coding: utf-8
 import os
 import shutil
-
-import utils
+import tom_lib.utils as utils
 from flask import Flask, render_template
-from nlp.topic_model import NonNegativeMatrixFactorization
-
-from tom_lib.structure import Corpus
+from tom_lib.nlp.topic_model import NonNegativeMatrixFactorization
+from tom_lib.structure.corpus import Corpus
 
 __author__ = "Adrien Guille"
 __email__ = "adrien.guille@univ-lyon2.fr"
 
 # Flask Web server
-app = Flask(__name__)
+app = Flask(__name__, static_folder='browser/static', template_folder='browser/templates')
 
 # Parameters
 max_tf = 0.8
 min_tf = 4
-lemmatizer = None
-num_topics = 20
+preprocessor = None
+num_topics = 15
 vectorization = 'tfidf'
 
 # Load corpus
-corpus = Corpus(source_file_path='../input/elysee.csv',
+corpus = Corpus(source_file_path='input/egc_lemmatized.csv',
                 language='french',
                 vectorization=vectorization,
                 max_relative_frequency=max_tf,
                 min_absolute_frequency=min_tf,
-                preprocessor=None)
+                preprocessor=preprocessor)
 print 'corpus size:', corpus.size
 print 'vocabulary size:', len(corpus.vocabulary)
 
@@ -37,33 +35,33 @@ topic_model.infer_topics(num_topics=num_topics)
 topic_model.print_topics(num_words=10)
 
 # Clean the data directory
-if os.path.exists('static/data'):
-    shutil.rmtree('static/data')
-os.makedirs('static/data')
+if os.path.exists('browser/static/data'):
+    shutil.rmtree('browser/static/data')
+os.makedirs('browser/static/data')
 
 # Export topic cloud
-utils.save_topic_cloud(topic_model, 'static/data/topic_cloud.json')
+utils.save_topic_cloud(topic_model, 'browser/static/data/topic_cloud.json')
 
 # Export details about topics
 for topic_id in range(topic_model.nb_topics):
     utils.save_word_distribution(topic_model.top_words(topic_id, 20),
-                                 'static/data/word_distribution' + str(topic_id) +'.tsv')
+                                 'browser/static/data/word_distribution' + str(topic_id) + '.tsv')
     utils.save_affiliation_repartition(topic_model.affiliation_repartition(topic_id),
-                                       'static/data/affiliation_repartition' + str(topic_id) +'.tsv')
+                                       'browser/static/data/affiliation_repartition' + str(topic_id) + '.tsv')
     evolution = []
     for i in range(2012, 2016):
         evolution.append((i, topic_model.topic_frequency(topic_id, date=i)))
-    utils.save_topic_evolution(evolution, 'static/data/frequency' + str(topic_id) + '.tsv')
+    utils.save_topic_evolution(evolution, 'browser/static/data/frequency' + str(topic_id) + '.tsv')
 
 # Export details about documents
 for doc_id in range(topic_model.corpus.size):
     utils.save_topic_distribution(topic_model.topic_distribution_for_document(doc_id),
-                                  'static/data/topic_distribution_d' + str(doc_id) +'.tsv')
+                                  'browser/static/data/topic_distribution_d' + str(doc_id) + '.tsv')
 
 # Export details about words
 for word_id in range(len(topic_model.corpus.vocabulary)):
     utils.save_topic_distribution(topic_model.topic_distribution_for_word(word_id),
-                                  'static/data/topic_distribution_w' + str(word_id) +'.tsv')
+                                  'browser/static/data/topic_distribution_w' + str(word_id) + '.tsv')
 
 # Associate documents with topics
 topic_associations = topic_model.documents_per_topic()
@@ -71,7 +69,7 @@ topic_associations = topic_model.documents_per_topic()
 # Export per-topic author network
 for topic_id in range(topic_model.nb_topics):
     utils.save_json_object(corpus.collaboration_network(topic_associations[topic_id]),
-                           'static/data/author_network' + str(topic_id) +'.json')
+                           'browser/static/data/author_network' + str(topic_id) + '.json')
 
 
 @app.route('/')
@@ -85,7 +83,7 @@ def index():
                            max_tf=max_tf,
                            min_tf=min_tf,
                            vectorization=vectorization,
-                           preprocessor=type(lemmatizer).__name__,
+                           preprocessor=type(preprocessor).__name__,
                            num_topics=num_topics)
 
 
@@ -177,5 +175,5 @@ def word_details(wid):
                            documents=documents)
 
 if __name__ == '__main__':
-    # Load corpus
+    # Access the browser at http://localhost:2016/
     app.run(debug=True, host='localhost', port=2016)

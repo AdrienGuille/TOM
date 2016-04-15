8bb26129d35fcf5db5a9a8b3d344467cf0a301fa
Pavel <edmundo.soriano-morales@univ-lyon2.fr>
2016-04-16 01:24:57 +0200

    MODIF: Bumped version again... Added download url to setup.py

--- setup.py
+++ setup.py
@@ -4,21 +4,26 @@ import tom_lib
 
 __author__ = "Adrien Guille, Pavel Soriano"
 __email__ = "adrien.guille@univ-lyon2.fr"
+version = tom_lib.__version__
 
 setup(
     name='tom_lib',
-    version=tom_lib.__version__,
+    version=version,
     packages=find_packages(),
     author="Adrien Guille, Pavel Soriano",
     author_email="adrien.guille@univ-lyon2.fr",
     description="A library for topic modeling and browsing",
     long_description=open('README.rst').read(),
     url='http://mediamining.univ-lyon2.fr/people/guille/tom.php',
+    download_url='http://pypi.python.org/packages/source/t/tom_lib/tom_lib-%s.tar.gz' % version,
     classifiers=[
         'Programming Language :: Python',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         "Operating System :: OS Independent",
         'Programming Language :: Python :: 2.7',
-        'Topic :: Scientific/Engineering'
+        'Topic :: Scientific/Engineering',
+        'Topic :: Text Processing'
     ])
+
+https://pypi.python.org/packages/source/t/tom_lib/tom_lib-%s.tar.gz
